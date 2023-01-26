0fc06b53beb6ea734064e28df9b1a561ed8ad69d
Raman Ebrahimi <Ramanebrahimi1998@gmail.com>
2023-01-25 20:17:49 -0500

    sldjfhbvldskhfbv

--- A1-7001-Jan26/hw1_prob2.m
+++ A1-7001-Jan26/hw1_prob2.m
@@ -1,19 +1,5 @@
-function [in,out] = binchan(p0,l1,l0)
-num = 5000;
-p1 = 1-p0;
-r = rand(num,1);
-in = (r<=p1); % source bits
-ind1 = find(in~=0); % locations of ones 
-len = length(ind1);
-r = rand(1,len);
-q = (r>l1); % zero if bit flip (1 -> 0), one if remains (1 -> 1)
-o1 = q;
-ind0 = find(in==0); % locations of zeros
-len = length(ind0);
-r = rand(1,len);
-q = (r<=l0); % one if bit flip (0 -> 1), zero if remains (0 -> 0)
-o0 = q;
-out = zeros(num,1);
-out(ind1) = o1;
-out(ind0) = o0;
-%% 
+a = binchan(0.3, 0.1, 0.2);
+b = binchan(0.3, 0.7, 0.2);
+c = binchan(0.8, 0.5, 0.6);
+d = binchan(0.5, 0.5, 0.5);
+
